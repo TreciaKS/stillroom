a8482bb306c11914a2583f59831188a4ed025a00
TreciaKS <82657928+TreciaKS@users.noreply.github.com>
2025-11-03 20:03:28 +0200

    feat: styled mission

--- client/app/components/Mission.tsx
+++ client/app/components/Mission.tsx
@@ -5,10 +5,10 @@ const Mission = () => {
       className="relative z-10 text-center px-8 py-2 bg-(--pure-white) w-full max-lg:pt-6"
     >
       <h2 className="text-3xl lg:text-6xl font-semibold text-(--pure-graphite) mb-4">Free. Forever.</h2>
-      <p className="text-neutral-400 max-w-2xl mx-auto text-lg leading-relaxed">
-        Stillroom isn&apos;t here to monetize curiosity. It&apos;s here to keep
-        the light on — to make understanding code accessible, quiet, and
-        permanent.
+      <p className="text-(--pure-gray) max-w-2xl mx-auto text-lg leading-relaxed">
+        Stillroom isn&apos;t here to monetize curiosity.
+        <br />
+        It&apos;s here to help you continue to learn, to understand code and to build up your confidence as a developer.
       </p>
     </section>
   );
