f592789f6711fe45619700ca4083c51a9b15a533
TreciaKS <82657928+TreciaKS@users.noreply.github.com>
2025-11-03 20:28:28 +0200

    refactor: remove double spelling

--- client/app/components/Footer.tsx
+++ client/app/components/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer className="relative bottom-0 w-full text-center py-6 text-xs text-(--pure-white) bg-(--pure-black) z-20">
-      © {new Date().getFullYear()} Stillroom. Made with with heart by @ TreciaKS
+      © {new Date().getFullYear()} Stillroom. Made with heart by @ TreciaKS
     </footer>
   );
 };
